824b7dee23833ffe7663a2492411ad5f660aaf69
chippew8 <106949305+chippew8@users.noreply.github.com>
2022-11-09 23:52:35 +0800

    new file

---
+++ MAD_Assignment/app/src/main/java/a21244w/com/example/mad_assignment/MainActivity.kt
@@ -0,0 +1,11 @@
+package a21244w.com.example.mad_assignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
