26e3a54ce6e89b1d68bd7f5305e43ed4f2991826
chippew8 <xuanxuan.apx@hotmail.com>
2022-11-14 11:57:02 +0800

    stuff

--- MovieReviewApplication/app/src/main/java/com/example/moviereviewapplication/MovieDetail.kt
+++ MovieReviewApplication/app/src/main/java/com/example/moviereviewapplication/MovieDetail.kt
@@ -3,9 +3,11 @@ package com.example.moviereviewapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MovieDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
+        var m = MovieClass("Venom", "When Eddie Brock acquires the powers of a symbiote, he will have to release his alter-ego Venom to save his life", "English","03-10-2018", "Yes")
     }
 }
