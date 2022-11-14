f0a3eff2f8446c6f6044dafb7ef89422d90f88e9
chippew8 <xuanxuan.apx@hotmail.com>
2022-11-14 15:12:18 +0800

    second activity done

--- MovieReviewApplication/app/src/main/java/com/example/moviereviewapplication/MovieDetail.kt
+++ MovieReviewApplication/app/src/main/java/com/example/moviereviewapplication/MovieDetail.kt
@@ -2,6 +2,8 @@ package com.example.moviereviewapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_movie_detail.*
 
 
 class MovieDetail : AppCompatActivity() {
@@ -9,5 +11,11 @@ class MovieDetail : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
         var m = MovieClass("Venom", "When Eddie Brock acquires the powers of a symbiote, he will have to release his alter-ego Venom to save his life", "English","03-10-2018", "Yes")
+
+        findViewById<TextView>(R.id.titleVal).text = m.name
+        findViewById<TextView>(R.id.overviewVal).text = m.desc
+        findViewById<TextView>(R.id.languageVal).text = m.language
+        findViewById<TextView>(R.id.dateVal).text = m.releaseDate
+        findViewById<TextView>(R.id.suitableVal).text = m.suitable
     }
 }
