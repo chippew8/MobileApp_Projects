1c60c964dda6e9e1af9548200e83789b278006cd
chippew8 <xuanxuan.apx@hotmail.com>
2022-11-14 11:00:12 +0800

    one activity done

--- MovieReviewApplication/app/src/main/java/com/example/moviereviewapplication/MainActivity.kt
+++ MovieReviewApplication/app/src/main/java/com/example/moviereviewapplication/MainActivity.kt
@@ -9,48 +9,96 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var suitableCategory: View
-    var movieName: String = ""
-    var movieDesc: String = ""
-    var movieLang: String = ""
-    var movieRelease: String = ""
+    var btnFeedback: String = ""
+    var movieName: String = "Title: "
+    var movieDesc: String = "\nOverview: "
+    var movieLang: String = "\nLanguage: "
+    var movieRelease: String = "\nRelease date = "
+    var notsuitable: String = "\nNot suitable for all ages = "
+    var reason: String = "\nReason: "
+
+    fun nameCheck(): Boolean {
+        if (NameInput.text.toString().isEmpty() == true){
+            NameInput.setError("Name cannot be empty")
+            return false
+        }else{
+            movieName += NameInput.text.toString()
+            return true
+        }
+    }
+    fun descCheck(): Boolean {
+        if (DescriptionInput.text.toString().isEmpty() == true){
+            DescriptionInput.setError("Description cannot be empty")
+            return false
+        }else{
+            movieDesc += DescriptionInput.text.toString()
+            return true
+        }
+    }
+    fun dateCheck(): Boolean {
+        if (ReleaseDateInput.text.toString().isEmpty() == true){
+            ReleaseDateInput.setError("Release date cannot be empty")
+            return false
+        }else{
+            movieRelease += ReleaseDateInput.text.toString()
+            return true
+        }
+    }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        suitableCategory.visibility = View.GONE
 
         languagePick.setOnCheckedChangeListener(object:RadioGroup.OnCheckedChangeListener{
 
             override fun onCheckedChanged(p0: RadioGroup?, p1: Int) {
                 if (p1 == R.id.englishCheck) {
-                    movieLang = "English"
+                    movieLang += "English"
                 } else if (p1 == R.id.chineseCheck) {
-                    movieLang = "Chinese"
+                    movieLang += "Chinese"
                 } else if (p1 == R.id.malayCheck) {
-                    movieLang = "Malay"
+                    movieLang += "Malay"
                 } else if (p1 == R.id.indianCheck) {
-                    movieLang = "Indian"
+                    movieLang += "Indian"
                 }
 
             }
         })
 
 
-//        chkUnsuitable.setOnClickListener {
-//            if(chkUnsuitable.isChecked == true)
-//        }
+        chkUnsuitable.setOnClickListener {
+            if(chkUnsuitable.isChecked == true){
+                ViolenceCheck.setVisibility(View.VISIBLE)
+                LanguageCheck.setVisibility(View.VISIBLE)
+                notsuitable += "true"
+
+                LanguageCheck.setOnClickListener {
+                    if (LanguageCheck.isChecked == true){
+                        reason += "\n Language"
+                    }
+                }
+                ViolenceCheck.setOnClickListener {
+                    if (ViolenceCheck.isChecked == true){
+                        reason += "\n Violence"
+                    }
+                }
+            }else if(chkUnsuitable.isChecked == false){
+                ViolenceCheck.setVisibility(View.GONE)
+                LanguageCheck.setVisibility(View.GONE)
+                notsuitable += "false"
+            }
+        }
+
+
 
         btnSubmit.setOnClickListener {
-
-            movieName = NameInput.text.toString()
-            movieDesc = DescriptionInput.text.toString()
-            movieRelease = ReleaseDateInput.text.toString()
-
-
-            displayToast("Title = " + movieName + " \n Overview = " + movieDesc + "\n Release Date = " +
-            movieRelease + "\n Language = " + movieLang)
+            if (nameCheck() || descCheck() || dateCheck()) {
+                btnFeedback += movieName + movieDesc + movieRelease + movieLang + notsuitable + reason
+                displayToast(btnFeedback)
+            }
         }
     }
 
---
+++ MovieReviewApplication/app/src/main/java/com/example/moviereviewapplication/MovieDetail.kt
@@ -0,0 +1,11 @@
+package com.example.moviereviewapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+    }
+}
